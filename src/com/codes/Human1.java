8b0121a0cbbe58b0165fcab5192d0773bcca55db
shilpa-p-a <shilpapa.cse@skit.org.in>
2025-11-19 21:57:50 +0530

    commit

---
+++ src/com/codes/Human1.java
@@ -0,0 +1,24 @@
+package com.codes;
+
+public class Human1 {
+		private int age;
+		private String name;
+		
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public int getAge() {
+			return age;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getName() {
+			return name;
+		}
+		
+		
+	}
+
+
