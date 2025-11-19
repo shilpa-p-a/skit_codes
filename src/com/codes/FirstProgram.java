893109764d6664d038b81078733cc708b289ddf6
shilpa-p-a <shilpapa.cse@skit.org.in>
2025-11-19 20:47:08 +0530

    initial commit

---
+++ src/com/codes/FirstProgram.java
@@ -0,0 +1,14 @@
+package com.codes;
+
+public class FirstProgram {
+
+	
+		
+			public static void main(String[] args) {
+				System.out.println("Hello World");
+
+			}
+
+		}
+		// TODO Auto-generated method stub
+
