667513364f8e3bd91bb63be702ee9cd12d70b659
shilpa-p-a <shilpapa.cse@skit.org.in>
2025-11-19 22:06:10 +0530

    commit

--- src/com/codes/FirstProgram.java
+++ src/com/codes/FirstProgram.java
@@ -1,12 +1,8 @@
 package com.codes;
 
-public class FirstProgram {
-
-	
-		
+public class FirstProgram {		
 			public static void main(String[] args) {
 				System.out.println("Hello World");
-
 			}
 
 		}
