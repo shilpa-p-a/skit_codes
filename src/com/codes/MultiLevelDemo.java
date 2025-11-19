e7f3bc85e2dc8bc29c74cf8aac099024e6808bf7
shilpa-p-a <shilpapa.cse@skit.org.in>
2025-11-19 22:12:22 +0530

    commit

---
+++ src/com/codes/MultiLevelDemo.java
@@ -0,0 +1,12 @@
+package com.codes;
+
+public class MultiLevelDemo {
+	public static void main(String[] args) {
+		SmartPhone obj=new SmartPhone ();
+		obj.deviceType();
+		obj.brand();
+		obj.feature();
+
+	}
+
+}
