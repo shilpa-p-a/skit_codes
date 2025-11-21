4ec972b1d2dbd5d5ee715278007a3ed6076743a3
shilpa-p-a <shilpapa.cse@skit.org.in>
2025-11-21 16:06:06 +0530

    c commit

--- src/com/codes/MethodOverridingDemo.java
+++ src/com/codes/MethodOverridingDemo.java
@@ -1,5 +1,28 @@
 package com.codes;
 
+ class Payment {
+		void makePayment()
+		{
+			System.out.println("Processing payment using generic method");
+		}
+	}
+
+	class UpiPayment extends Payment //sub class
+	{
+		void makePayment()
+		{
+			System.out.println("Payment made using UPI");
+		}
+	}
+	class CardPayment extends Payment //sub class
+	{
+		void makePayment()
+		{
+			System.out.println("Payment made using credit card");
+		}
+	}
+
+
 public class MethodOverridingDemo {
 	public static void main(String[] args) {
 		Payment payment;
